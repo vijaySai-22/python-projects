f7a0f3a1438a2296df358a78f68b1040514f263d
Pujari Vijay Sai Prasad <62492984+vijaySai-22@users.noreply.github.com>
2021-07-01 13:37:15 +0530

    updated calculate code
    now this code can get data and calculate data give result

--- billing/bill.py
+++ billing/bill.py
@@ -15,50 +15,67 @@ class App():
         self.phno_entry=Entry(root,font=('Arial',18),width=(20)).place(x=769,y=120)
         Label(root,text="Enter Address",font=('Arial',18)).place(x=1045,y=120)
         c_add=tk.StringVar()
-        self.add_entry=Entry(root,font=('Arial',18),width=(36)).place(x=1210,y=120)
-        Label(root,text="Age",font=('Arial',18)).place(x=1695,y=120)
-        c_age=tk.StringVar()
-        self.age_entry=Entry(root,font=('Arial',18),width=(4)).place(x=1750,y=120)
+        self.add_entry=Entry(root,font=('Arial',18),width=(46)).place(x=1210,y=120)
+        # Label(root,text="Age",font=('Arial',18)).place(x=1695,y=120)
+        # c_age=tk.StringVar()
+        # self.age_entry=Entry(root,font=('Arial',18),width=(4)).place(x=1750,y=120)
 
         ##########---------------Particulars-------------###########
 
         Label(root,text="Particulars",font=('Arial',30),fg='white',bg='black',width=(50)).place(x=5,y=180)
         self.veg=['Potato','Tomato','Carrot','Brinjal','Onions','Cabage','Ginger','Chilli','Garlic','Beetroot']
-        #self.veg_price=[20,22,30,25,30,30,70,32,60,53]
+        self.veg_price=[20,22,30,25,30,30,70,32,60,53]
         self.ck=[None]*len(self.veg)
         self.qty=[None]*len(self.veg)
         self.dis=[None]*len(self.veg)
+        self.each_price=[None]*len(self.veg)
         self.y_value=300
-        Label(root,text="ItemNo     Vegetables              Qty         Discount        GST     Total                 ",font=('Arial',23)).place(x=10,y=240)
+        Label(root,text="   ItemNo         Vegetables                           Qty          Discount          Total      ",font=('Arial',23)).place(x=10,y=240)
         for i in range(len(self.veg)):
-            self.item_no='Item'+str(i)
-            Label(root,text=self.item_no,font=('Arial',23)).place(x=10,y=self.y_value)
+            self.item_no='Item'+str(i+1)
+            Label(root,text=self.item_no,font=('Arial',23)).place(x=50,y=self.y_value)
 
-            self.ck[i]=tk.StringVar()
+            self.ck[i]=tk.IntVar()
             name = Checkbutton(root, text = self.veg[i],variable = self.ck[i],
                     onvalue = 1,offvalue = 0,
-                    font=('Arial',23),width = 13,anchor="w"
-                    ).place(x=130,y=self.y_value)
+                    font=('Arial',23),width = 18,anchor="w"
+                    ).place(x=180,y=self.y_value)
 
-            self.qty[i]=tk.StringVar()
-            Entry(root,font=('Arial',23),textvariable=self.qty[i],width=2).place(x=430,y=self.y_value)
-            Label(root,text=".kg",font=('Arial',23)).place(x=480,y=self.y_value)
+            self.qty[i]=tk.DoubleVar()
+            self.qty[i].set(0)
+            Entry(root,font=('Arial',23),textvariable=self.qty[i],width=3).place(x=600,y=self.y_value)
+            Label(root,text=".kg",font=('Arial',23)).place(x=660,y=self.y_value)
 
-            self.dis[i]=tk.StringVar()
-            Entry(root,font=('Arial',23),textvariable=self.dis[i],width=2).place(x=580,y=self.y_value)
-            Label(root,text="% ",font=('Arial',23)).place(x=630,y=self.y_value)
+            self.dis[i]=tk.DoubleVar()
+            self.dis[i].set(0)
+            Entry(root,font=('Arial',23),textvariable=self.dis[i],width=3).place(x=760,y=self.y_value)
+            Label(root,text="% ",font=('Arial',23)).place(x=820,y=self.y_value)
 
-            Label(root,text="18% ",font=('Arial',23)).place(x=750,y=self.y_value)
-            Label(root,text="0 Rs",font=('Arial',23)).place(x=880,y=self.y_value)
+            self.each_price[i]=Label(root,text="0 Rs",font=('Arial',23))
+            self.each_price[i].place(x=980,y=self.y_value)
             self.y_value=self.y_value+60
-        self.cal=Button(root,text="Caculate",font=('Arial',23),command=self.calculate).place(x=850,y=900)
+        self.cal=Button(root,text="Total=",font=('Arial',23),command=self.calculate).place(x=760,y=900)
+        self.total_price=Label(root,text="0 RS",font=('Arial',27),width=9)
+        self.total_price.place(x=900,y=900)
         ##########-----------line-------------##########
         Label(root,font=(30),bg='black',width=(3),height=(40)).place(x=1100,y=180)
         ##########-----------Bill-------------##########
-        bill_heading=Label(root,text="Bill",font=('Arial',30),fg='white',bg='black',width=(32)).place(x=1130,y=180)
+        Label(root,text="Bill",font=('Arial',30),fg='white',bg='black',width=(32)).place(x=1130,y=180)
         complete_and_save=Button(root,text="Complete and Save",font=('Arial',23)).place(x=1500,y=900)
     def calculate(self):
-        pass
+        tot=0.0
+        for i in range(len(self.veg)):
+            each_tot=0.0
+            if self.ck[i].get()==1 and self.qty[i].get()>0:
+                each_tot=each_tot+self.veg_price[i]*self.qty[i].get()
+                each_tot=each_tot-((each_tot/100)*self.dis[i].get())
+                self.each_price[i].config(text=str(each_tot)+" Rs")
+                tot=tot+each_tot
+                self.total_price.config(text=str(tot)+" Rs")
+            else:
+                self.each_price[i].config(text="0 Rs")
+                self.qty[i].set(0)
+                self.dis[i].set(0)
 if __name__ == "__main__":
     win=tk.Tk()
     #title
