262eb0bd4fd5321bb3a6b8b5939acdd82b5a40a1
Pujari Vijay Sai Prasad <62492984+vijaySai-22@users.noreply.github.com>
2021-07-17 23:52:11 +0530

    Added: Rating of password
    and fixed minor bugs:
    hiding copy btn and rating after changing input more than 12 and input is blank

--- projects/password-generator.py
+++ projects/password-generator.py
@@ -11,11 +11,12 @@ class App:
         f1.place(x=0,y=50,relwidth=1)
         Label(f1,text="Enter number of characters: ").grid(row=0,column=0,padx=10,pady=10)
         self.n=tk.StringVar()
-        self.n_Entry=Entry(f1,textvariable=self.n).grid(row=0,column=1,padx=10,pady=10)
-        Button(f1,text="Generate",command=self.gen).grid(row=1,column=0,columnspan=2)
+        self.n_Entry=Entry(f1,textvariable=self.n).grid(row=0,column=1,columnspan=2,padx=10,pady=10)
+        Button(f1,text="Generate",command=self.gen).grid(row=1,column=0,columnspan=3)
 
         self.pswrd=Text(f1)
         self.cpyBtn=Button(f1,text="Copy",command=self.cpy)
+        self.rateLbl=Label(f1)
     def gen(self):
         if self.n.get().isnumeric() and int(self.n.get())>=4 and int(self.n.get())<=12:
             self.ucAlpha="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -37,14 +38,35 @@ class App:
             self.pswrd.grid(row=2,column=0)
             #cpy btn
             self.cpyBtn.grid(row=2,column=1)
+
+            #rating
+            self.x=u'\u26AB'
+            self.y=u'\u26AA'
+            self.rate=''
+            if int(self.n.get())>=4 and int(self.n.get())<6:
+                self.rate=self.rate+self.x+self.y*4
+            elif int(self.n.get())>=6 and int(self.n.get())<8:
+                self.rate=self.rate+self.x*2+self.y*3
+            elif int(self.n.get())>=8 and int(self.n.get())<10:
+                self.rate=self.rate+self.x*3+self.y*2
+            elif int(self.n.get())>=10 and int(self.n.get())<12:
+                self.rate=self.rate+self.x*4+self.y
+            else:
+                self.rate=self.rate+self.x*5
+            self.rateLbl.config(text=self.rate)
+            self.rateLbl.grid(row=2,column=2)
         elif self.n.get()=='':
-            pass
+            self.cpyBtn.grid_forget()
+            self.rateLbl.grid_forget()
+            self.pswrd.grid_forget()
         else:
             self.pswrd.config(state='normal')
             self.pswrd.delete('0.0','end')
             self.pswrd.insert('insert','Enter valid input(eg: 4,5 upto 12)')
             self.pswrd.config(width=35,height=1,state='disabled')
             self.pswrd.grid(row=2,column=0,columnspan=2)
+            self.cpyBtn.grid_forget()
+            self.rateLbl.grid_forget()
             #msgbox
             messagebox.showwarning("Warning", "Enter valid input")
     def cpy(self):
